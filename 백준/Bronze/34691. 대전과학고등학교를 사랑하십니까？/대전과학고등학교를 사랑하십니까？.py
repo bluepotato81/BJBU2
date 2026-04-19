17619bf2944a2d8ab6af61f938327c4dcfa51f78
bluepotato81 <164982578+bluepotato81@users.noreply.github.com>
2026-04-19 22:24:53 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,10 @@
+while True:
+    k=input()
+    if k=="end":
+        break
+    if k=="animal":
+        print("Panthera tigris")
+    elif k=="tree":
+        print("Pinus densiflora")
+    elif k=="flower":
+        print("Forsythia koreana")
