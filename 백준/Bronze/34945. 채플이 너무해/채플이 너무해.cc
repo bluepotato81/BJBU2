8f2575fb45a355906e81f8888a1aa834d7c16eeb
bluepotato81 <164982578+bluepotato81@users.noreply.github.com>
2026-04-19 22:22:00 +0900

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int in;
+    scanf("%d", &in);
+    if(in<=5) {
+        printf("Oh My God!");
+    }
+    else {
+        printf("Success!");
+    }
+    return 0;
+}
