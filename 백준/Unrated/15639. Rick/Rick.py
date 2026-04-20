ec35dd687e9e4637b5a9200b6bd6c65d182de50a
bluepotato81 <164982578+bluepotato81@users.noreply.github.com>
2026-04-20 18:56:36 +0900

    [Unrated] Title: Rick, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Unrated/15639. Rick/Rick.py
@@ -0,0 +1,24 @@
+line = input()
+
+if line == "Give you up":
+    print("NO")
+elif line == "Let you down":
+    print("NO")
+elif line == "Run around":
+    print("NO")
+elif line == "Desert you":
+    print("NO")
+elif line == "Run around and desert you":
+    print("NO")
+elif line == "Make you cry":
+    print("NO")
+elif line == "Say goodbye":
+    print("NO")
+elif line == "Tell a lie":
+    print("NO")
+elif line == "Hurt you":
+    print("NO")
+elif line == "Tell a lie and hurt you":
+    print("NO")
+else:
+    print("YES")
