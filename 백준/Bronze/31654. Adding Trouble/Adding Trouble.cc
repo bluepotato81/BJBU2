72cec493d27e50df8586162b5b9959c515f0c958
bluepotato81 <164982578+bluepotato81@users.noreply.github.com>
2026-04-20 18:43:01 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,8 @@
+#import<ios>
+int a,b,c;main(){scanf("%d%d%d",&a,&b,&c);
+                if(a+b==c) {
+                    printf("correct!");
+                }
+                else{
+                    printf("wrong!");
+                }}
