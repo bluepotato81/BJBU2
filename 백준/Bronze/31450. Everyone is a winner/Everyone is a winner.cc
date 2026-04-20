8a4c7af89500bcc093627cd89f1b3d883eb80925
bluepotato81 <164982578+bluepotato81@users.noreply.github.com>
2026-04-20 18:43:50 +0900

    [Bronze V] Title: Everyone is a winner, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.cc
@@ -0,0 +1,9 @@
+#import<ios>
+int a,b;main(){scanf("%d %d",&a,&b);
+         if(!(a%b)) {
+             printf("Yes");
+         }
+         else {
+             printf("No");
+         }
+              }
